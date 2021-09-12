37ff76545fda021f0ad8ce3e8144fd8ccc00ff80
tomoyanp <tomoyanpy@gmail.com>
2021-09-12 15:54:08 +0900

    m

--- webapp/sql/init.sh
+++ webapp/sql/init.sh
@@ -3,11 +3,11 @@ set -xe
 set -o pipefail
 
 CURRENT_DIR=$(cd $(dirname $0);pwd)
-export MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
-export MYSQL_PORT=${MYSQL_PORT:-3306}
-export MYSQL_USER=${MYSQL_USER:-isucari}
-export MYSQL_DBNAME=${MYSQL_DBNAME:-isucari}
-export MYSQL_PWD=${MYSQL_PASS:-isucari}
+export MYSQL_HOST=isucon03
+export MYSQL_PORT=3306
+export MYSQL_USER=isucari
+export MYSQL_DBNAME=isucari
+export MYSQL_PWD=isucari
 export LANG="C.UTF-8"
 cd $CURRENT_DIR
 
